348172f88cbf6461cc86f8e9c1b86167a0e0e17c
Victor <victoe680@gmail.com>
2020-11-17 19:44:41 -0600

    conexion db

--- models/db_conexion.js
+++ models/db_conexion.js
@@ -1,9 +1,9 @@
 const {Client} = require('pg')
 const connectionData = {
-    user: 'postgres',
-    host: 'localhost',
-    database: 'estudiantes',
-    password: '123',
+    user: 'vllurhyopsaqpr',
+    host: 'ec2-3-210-23-22.compute-1.amazonaws.com',
+    database: 'd1li1r31987ipj',
+    password: '63b6badd697c04285a388abdf37abe57f0d1bc51651d2cbf6833d0c9c02db0ce',
     port: 5432,
 }
 
